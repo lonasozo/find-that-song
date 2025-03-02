ae77c8fc18c45a6f859483117e26e242d6fc380e
Lorenz <lorenzz@duck.com>
2025-03-02 18:33:08 +0100

    feat: add Vercel analytics integration and update package dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import { Analytics } from '@vercel/analytics/react';
-import '../styles/globals.css'; // Assumendo che tu abbia questo import (se non esiste, rimuovilo)
 
 function MyApp({ Component, pageProps }) {
   return (
